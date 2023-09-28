1e523fdc0e62828ac5a1ab0b1b1fc65d60eaff3b
Jean-Charles Lateltin <jeancharles.lateltin@dawex.com>
2023-09-28 13:34:45 +0200

    deploy generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  dir: {
+    public: 'docs' // not sure,
+
+  },
   app: {
     head: {
+      title: 'Ou regarder le match',
+      meta: {}, // remplir plus tard
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lora&display=swap' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' }
